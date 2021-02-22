66a6a054d46425fe248a56171a6521b14b09be09
thanhdt246 <dothanh24062000@gmail.com>
2021-02-22 16:35:24 +0700

    edit sigup.ts

--- src/app/component/signin-signup/signup/signup.component.ts
+++ src/app/component/signin-signup/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../../service/auth.service";
 import {Router} from "@angular/router";
+import {ProvinceService} from "../../../service/province.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -9,14 +11,25 @@ import {Router} from "@angular/router";
 })
 export class SignupComponent implements OnInit {
 
+  signupForm: FormGroup = this.formBuilder.group({});
+
   constructor(private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private provinceService: ProvinceService,
+              private formBuilder: FormBuilder) {
     if (this.authService.currentUserValue) {
       this.router.navigate(['/']);
     }
   }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      username: ['',[Validators.required,Validators.minLength(6)]],
+      password: ['',[Validators.required,Validators.minLength(6)]],
+      phoneNumber: ['',[Validators.required,Validators.minLength(10),Validators.maxLength(10)]],
+      address: ['',[Validators.required]]
+    })
+
   }
 
 }
